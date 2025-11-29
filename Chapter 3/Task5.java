34fbae5e8ea988c8f4764901f3d5727ff15b146d
Aadrian15 <atams1xxx@gmail.com>
2025-11-29 01:47:14 +0000

    Task

--- Chapter 3/Task5.java
+++ Chapter 3/Task5.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class Task5 {
 
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter three numbers:");
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        int n3 = s.nextInt();
+        s.close();
+
+        if (n1 == n2 && n2 == n3) {
+            System.out.println("All numbers are equal");
+        } else {
+            // Find the maximum of all three numbers in one line
+            int largest = Math.max(n1, Math.max(n2, n3));
+            System.out.println("The largest number is: " + largest);
+        }
+    }
 }
